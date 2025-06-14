159df70957c903c8b014027632caa02c546a0ee4
Haruki Nakajima <springtechtree@gmail.com>
2025-06-14 17:47:26 +0900

    fix: rename repr variable

--- pydefect/cli/main_print_json.py
+++ pydefect/cli/main_print_json.py
@@ -8,16 +8,16 @@ from monty.serialization import loadfn
 def main():
     if sys.argv[1] == "repr":
         filenames = sys.argv[2:]
-        repr = True
+        use_repr = True
     else:
         filenames = sys.argv[1:]
-        repr = False
+        use_repr = False
 
     for filename in filenames:
         print("-"*80)
         print(f"file: {filename}")
         obj = loadfn(filename)
-        print(obj.__repr__()) if repr else print(obj.__str__())
+        print(obj.__repr__()) if use_repr else print(obj.__str__())
 
 
 if __name__ == "__main__":
